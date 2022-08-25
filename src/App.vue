7d75ca0f7298b2ebd7ce75cfcab9f93eb0949245
Albin Norback <albin.nojback@gmail.com>
2022-08-25 13:40:26 +0200

    rm

--- src/App.vue
+++ src/App.vue
@@ -4,156 +4,161 @@ import ModalPopup from "@/components/ModalPopup.vue";
 </script>
 
 <template>
-  <div class="form-container">
-    <div class="input-container">
-      <label for="rows">Rows:</label>
-      <label for="cols">Columns:</label>
-      <label for="bomb-count">Bomb Count:</label>
-      <input v-model="rows" type="text" id="rows" />
-      <input v-model="cols" type="text" id="cols" />
-      <input v-model="bombCount" type="text" id="bomb-count" />
+    <div class="form-container">
+        <div class="input-container">
+            <label for="rows">Rows:</label>
+            <label for="cols">Columns:</label>
+            <label for="bomb-count">Bomb Count:</label>
+            <input v-model="rows" type="text" id="rows" />
+            <input v-model="cols" type="text" id="cols" />
+            <input v-model="bombCount" type="text" id="bomb-count" />
+        </div>
+        <button type="button" @click="startGame" class="submit-btn">
+            Start
+        </button>
     </div>
-    <button type="button" @click="startGame" class="submit-btn">Start</button>
-  </div>
-  <div class="label-container">
-    <h2>{{ timerLabel }}</h2>
-    <h2>{{ flagCount }} 🚩</h2>
-  </div>
+    <div class="label-container">
+        <h2>{{ timerLabel }}</h2>
+        <h2>{{ flagCount }} 🚩</h2>
+    </div>
 
-  <main>
-    <MineGrid
-      height="80vh"
-      ref="minefield"
-      @toggle-flag="handleToggleFlag"
-      @finish-game="handleFinishGame"
-    />
-  </main>
+    <main>
+        <MineGrid
+            height="80vh"
+            ref="minefield"
+            @toggle-flag="handleToggleFlag"
+            @finish-game="handleFinishGame"
+        />
+    </main>
 
-  <!-- <div class="gameover-popup">
+    <!-- <div class="gameover-popup">
     <h2>{{ gameoverIsWin ? "You win!" : "You lose!" }}</h2>
   </div> -->
 
-  <ModalPopup
-    :show="showPopup"
-    :win="isWin"
-    :seconds="timerSeconds"
-    @close="showPopup = false"
-  />
+    <ModalPopup
+        :show="showPopup"
+        :win="isWin"
+        :seconds="timerSeconds"
+        @close="showPopup = false"
+    />
 </template>
 
 <script lang="ts">
 import moment from "moment";
 
 export default {
-  data() {
-    return {
-      rows: 20,
-      cols: 20,
-      bombCount: 20,
-      flagCount: 20,
-      timerSeconds: 0,
-      timer: -1,
-      isWin: false,
-      showPopup: false,
-    };
-  },
-  methods: {
-    handleToggleFlag(patch: any) {
-      this.flagCount += patch.isFlagged ? -1 : +1;
+    data() {
+        return {
+            rows: 20,
+            cols: 20,
+            bombCount: 20,
+            flagCount: 20,
+            timerSeconds: 0,
+            timer: -1,
+            isWin: false,
+            showPopup: false,
+        };
     },
+    methods: {
+        handleToggleFlag(patch: any) {
+            this.flagCount += patch.isFlagged ? -1 : +1;
+        },
 
-    handleFinishGame(isWin: boolean) {
-      this.stopTimer();
-      this.showPopup = true;
-      this.isWin = isWin;
-    },
+        handleFinishGame(isWin: boolean) {
+            this.stopTimer();
+            this.showPopup = true;
+            this.isWin = isWin;
+        },
 
-    startGame() {
-      (this.$refs.minefield as any).setupGrid(
-        this.rows,
-        this.cols,
-        this.bombCount
-      );
+        startGame() {
+            (this.$refs.minefield as any).setupGrid(
+                this.rows,
+                this.cols,
+                this.bombCount
+            );
 
-      this.flagCount = parseInt(this.bombCount as any);
-      this.startTimer();
-    },
+            this.flagCount = parseInt(this.bombCount as any);
+            this.startTimer();
+        },
 
-    startTimer() {
-      this.timerSeconds = 0;
-      clearInterval(this.timer);
-      this.timer = setInterval(() => {
-        this.timerSeconds++;
-      }, 1000);
-    },
+        startTimer() {
+            this.timerSeconds = 0;
+            clearInterval(this.timer);
+            this.timer = setInterval(() => {
+                this.timerSeconds++;
+            }, 1000);
+        },
 
-    stopTimer() {
-      clearInterval(this.timer);
+        stopTimer() {
+            clearInterval(this.timer);
+        },
     },
-  },
-  computed: {
-    timerLabel() {
-      return moment().startOf("hour").second(this.timerSeconds).format("mm:ss");
+    computed: {
+        timerLabel() {
+            return moment()
+                .startOf("hour")
+                .second(this.timerSeconds)
+                .format("mm:ss");
+        },
     },
-  },
-  mounted() {
-    this.startGame();
-  },
+    mounted() {
+        this.startGame();
+    },
 };
 </script>
 
 <style>
 * {
-  padding: 0;
-  margin: 0;
+    padding: 0;
+    margin: 0;
 }
 
 #app {
-  min-height: 100vh;
-  width: 100vw;
-  display: grid;
-  grid-template-rows: 10vh 8vh 82vh;
-  justify-items: center;
+    min-height: 100vh;
+    width: 100vw;
+    display: grid;
+    grid-template-rows: 10vh 8vh 82vh;
+    justify-items: center;
 }
 
 main {
-  grid-row-start: 3;
+    grid-row-start: 3;
 }
 
 .form-container {
-  display: flex;
-  align-items: flex-start;
+    display: flex;
+    align-items: flex-start;
 }
 
 .input-container {
-  display: grid;
-  grid-template-rows: 1fr 1fr;
-  grid-template-columns: 1fr 1fr 1fr;
-  gap: 0.3rem;
+    display: grid;
+    grid-template-rows: 1fr 1fr;
+    grid-template-columns: 1fr 1fr 1fr;
+    gap: 0.3rem;
 }
 
 label {
-  font-weight: bold;
-  font-size: 1.2rem;
-  margin-left: 0.2rem;
+    font-weight: bold;
+    font-size: 1.2rem;
+    margin-left: 0.2rem;
 }
 
 .submit-btn {
-  display: inline;
-  align-self: center;
-  margin: 0.5rem;
-  padding: 0.3rem;
+    display: inline;
+    align-self: center;
+    margin: 0.5rem;
+    padding: 0.3rem;
 }
 
 .label-container {
-  text-align: center;
+    text-align: center;
 }
 
 .gameover-popup {
-  position: fixed;
-  left: 50%;
-  top: 50%;
-  transform: translate(-50%, -50%);
-  background-color: antiquewhite;
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    background-color: antiquewhite;
 }
 </style>
--- src/components/MineGrid.vue
+++ src/components/MineGrid.vue
@@ -1,278 +1,286 @@
 <template>
-  <div
-    class="minefield"
-    v-bind:style="
-      '--num-cols:' + columns + '; --num-rows:' + rows + '; --height:' + height
-    "
-  >
-    <button
-      v-for="(patch, index) in patches"
-      type="button"
-      :class="[
-        { bomb: patch.isBomb, revealed: patch.isRevealed },
-        labelClass(patch),
-      ]"
-      @click="patchClick(patch)"
-      @contextmenu.prevent="toggleFlag(patch)"
+    <div
+        class="minefield"
+        v-bind:style="
+            '--num-cols:' +
+            columns +
+            '; --num-rows:' +
+            rows +
+            '; --height:' +
+            height
+        "
     >
-      {{ patchLabel(patch) }}
-    </button>
-  </div>
+        <button
+            v-for="(patch, index) in patches"
+            type="button"
+            :class="[
+                { bomb: patch.isBomb, revealed: patch.isRevealed },
+                labelClass(patch),
+            ]"
+            @click="patchClick(patch)"
+            @contextmenu.prevent="toggleFlag(patch)"
+        >
+            {{ patchLabel(patch) }}
+        </button>
+    </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
 
 export default defineComponent({
-  data() {
-    return {
-      patches: [] as any[],
-      bombCount: 0,
-      rows: 0,
-      columns: 0,
-
-      bombPositions: [] as any[],
-      revealedSquareCount: 0,
-      gameover: false,
-      playerAlreadyClicked: false,
-    };
-  },
-
-  methods: {
-    setupGrid(rows: number, columns: number, bombCount: number) {
-      //set row and column to param
-      this.rows = rows;
-      this.columns = columns;
-      this.bombCount = bombCount;
-      this.patches = [];
-      this.bombPositions = [];
-      this.revealedSquareCount = 0;
-      this.gameover = false;
-      this.playerAlreadyClicked = false;
-
-      //instantiate the empty grid
-      for (let y = 0; y < rows; y++) {
-        for (let x = 0; x < columns; x++) {
-          this.patches[this.getIndex(x, y)] = {
-            x: x,
-            y: y,
-            isBomb: false,
-            isRevealed: false,
-            isFlagged: false,
-            neighbors: [],
+    data() {
+        return {
+            patches: [] as any[],
             bombCount: 0,
-          };
-        }
-      }
+            rows: 0,
+            columns: 0,
 
-      // console.log(this.patches);
-
-      //Create bombs
-      for (let i = 0; i < this.bombCount; i++) {
-        this.createBomb();
-      }
-
-      //Setup bombcounts and neighbors
-      this.patches.forEach((patch) => {
-        if (patch.isBomb) return;
-
-        // console.log(patch.neighbors);
-        // console.log(this.fetchNeighbors(patch.x, patch.y));
-
-        patch.neighbors = this.fetchNeighbors(patch.x, patch.y);
-
-        let bombCount = 0;
-        patch.neighbors.forEach((neighbor: any) => {
-          if (neighbor.isBomb) bombCount++;
-        });
-
-        patch.bombCount = bombCount;
-      });
-    },
-
-    getPatch(x: number, y: number) {
-      return this.patches[this.getIndex(x, y)];
-    },
-    getIndex(x: number, y: number) {
-      return y * this.columns + x;
-    },
-
-    fetchNeighbors(x: number, y: number) {
-      let neighbors = [];
-      for (let tempY = y - 1; tempY <= y + 1; tempY++) {
-        //If tempY is valid, loop through the x
-        if (tempY >= 0 && tempY < this.rows) {
-          for (let tempX = x - 1; tempX <= x + 1; tempX++) {
-            //If it is not the original position, continue
-            if (tempX === x && tempY === y) continue;
-
-            //If tempX is valid, add the position
-            if (tempX >= 0 && tempX < this.columns) {
-              neighbors.push(this.getPatch(tempX, tempY));
-            }
-          }
-        }
-      }
-
-      return neighbors;
-    },
-
-    createBomb() {
-      let pos = {
-        x: Math.floor(Math.random() * this.columns),
-        y: Math.floor(Math.random() * this.rows),
-      };
-
-      //Make sure the random position doesn't have a bomb
-      while (this.getPatch(pos.x, pos.y).isBomb === true) {
-        pos = {
-          x: Math.floor(Math.random() * this.columns),
-          y: Math.floor(Math.random() * this.rows),
+            bombPositions: [] as any[],
+            revealedSquareCount: 0,
+            gameover: false,
+            playerAlreadyClicked: false,
         };
-      }
-
-      //Debugging only probably
-      this.bombPositions.push(pos);
-
-      //Add the bomb
-      this.getPatch(pos.x, pos.y).isBomb = true;
     },
 
-    toggleFlag(patch: any) {
-      if (patch.isRevealed || this.gameover) return;
-      patch.isFlagged = !patch.isFlagged;
-      this.$emit("toggleFlag", patch);
+    methods: {
+        setupGrid(rows: number, columns: number, bombCount: number) {
+            //set row and column to param
+            this.rows = rows;
+            this.columns = columns;
+            this.bombCount = bombCount;
+            this.patches = [];
+            this.bombPositions = [];
+            this.revealedSquareCount = 0;
+            this.gameover = false;
+            this.playerAlreadyClicked = false;
+
+            //instantiate the empty grid
+            for (let y = 0; y < rows; y++) {
+                for (let x = 0; x < columns; x++) {
+                    this.patches[this.getIndex(x, y)] = {
+                        x: x,
+                        y: y,
+                        isBomb: false,
+                        isRevealed: false,
+                        isFlagged: false,
+                        neighbors: [],
+                        bombCount: 0,
+                    };
+                }
+            }
+
+            // console.log(this.patches);
+
+            //Create bombs
+            for (let i = 0; i < this.bombCount; i++) {
+                this.createBomb();
+            }
+
+            //Setup bombcounts and neighbors
+            this.patches.forEach((patch) => {
+                if (patch.isBomb) return;
+
+                // console.log(patch.neighbors);
+                // console.log(this.fetchNeighbors(patch.x, patch.y));
+
+                patch.neighbors = this.fetchNeighbors(patch.x, patch.y);
+
+                let bombCount = 0;
+                patch.neighbors.forEach((neighbor: any) => {
+                    if (neighbor.isBomb) bombCount++;
+                });
+
+                patch.bombCount = bombCount;
+            });
+        },
+
+        getPatch(x: number, y: number) {
+            return this.patches[this.getIndex(x, y)];
+        },
+        getIndex(x: number, y: number) {
+            return y * this.columns + x;
+        },
+
+        fetchNeighbors(x: number, y: number) {
+            let neighbors = [];
+            for (let tempY = y - 1; tempY <= y + 1; tempY++) {
+                //If tempY is valid, loop through the x
+                if (tempY >= 0 && tempY < this.rows) {
+                    for (let tempX = x - 1; tempX <= x + 1; tempX++) {
+                        //If it is not the original position, continue
+                        if (tempX === x && tempY === y) continue;
+
+                        //If tempX is valid, add the position
+                        if (tempX >= 0 && tempX < this.columns) {
+                            neighbors.push(this.getPatch(tempX, tempY));
+                        }
+                    }
+                }
+            }
+
+            return neighbors;
+        },
+
+        createBomb() {
+            let pos = {
+                x: Math.floor(Math.random() * this.columns),
+                y: Math.floor(Math.random() * this.rows),
+            };
+
+            //Make sure the random position doesn't have a bomb
+            while (this.getPatch(pos.x, pos.y).isBomb === true) {
+                pos = {
+                    x: Math.floor(Math.random() * this.columns),
+                    y: Math.floor(Math.random() * this.rows),
+                };
+            }
+
+            //Debugging only probably
+            this.bombPositions.push(pos);
+
+            //Add the bomb
+            this.getPatch(pos.x, pos.y).isBomb = true;
+        },
+
+        toggleFlag(patch: any) {
+            if (patch.isRevealed || this.gameover) return;
+            patch.isFlagged = !patch.isFlagged;
+            this.$emit("toggleFlag", patch);
+        },
+
+        patchClick(patch: any) {
+            if (patch.isFlagged || this.gameover) return;
+
+            if (!this.playerAlreadyClicked && patch.isBomb) {
+                let pos = { x: patch.x, y: patch.y };
+
+                while (this.getPatch(pos.x, pos.y).isBomb) {
+                    this.setupGrid(this.rows, this.columns, this.bombCount);
+                }
+
+                this.playerAlreadyClicked = true;
+                patch = this.getPatch(pos.x, pos.y);
+            }
+
+            if (patch.isBomb === true) {
+                console.log("Fail!");
+                this.revealAllBombs();
+                this.$emit("finishGame", false);
+            } else {
+                this.revealPatch(patch);
+            }
+        },
+
+        revealPatch(patch: any) {
+            if (patch.isFlagged || patch.isRevealed) return;
+
+            patch.isRevealed = true;
+            this.revealedSquareCount++;
+            if (
+                this.revealedSquareCount ===
+                this.patches.length - this.bombCount
+            ) {
+                this.$emit("finishGame", true);
+            }
+            if (patch.bombCount === 0) {
+                patch.neighbors.forEach((neighbor: any) => {
+                    this.revealPatch(neighbor);
+                });
+            }
+        },
+
+        patchLabel(patch: any) {
+            if (patch.isFlagged) return "🚩";
+
+            if (patch.isRevealed === false) return "";
+
+            if (patch.isBomb) return "💣";
+
+            if (patch.bombCount === 0) return "";
+
+            return patch.bombCount;
+        },
+
+        labelClass(patch: any) {
+            if (!patch.isRevealed) return "";
+
+            return "label-" + patch.bombCount;
+        },
+
+        revealAllBombs() {
+            this.bombPositions.forEach((pos) => {
+                if (!this.getPatch(pos.x, pos.y).isFlagged)
+                    this.getPatch(pos.x, pos.y).isRevealed = true;
+            });
+        },
     },
 
-    patchClick(patch: any) {
-      if (patch.isFlagged || this.gameover) return;
-
-      if (!this.playerAlreadyClicked && patch.isBomb) {
-        let pos = { x: patch.x, y: patch.y };
-
-        while (this.getPatch(pos.x, pos.y).isBomb) {
-          this.setupGrid(this.rows, this.columns, this.bombCount);
-        }
-
-        this.playerAlreadyClicked = true;
-        patch = this.getPatch(pos.x, pos.y);
-      }
-
-      if (patch.isBomb === true) {
-        console.log("Fail!");
-        this.revealAllBombs();
-        this.$emit("finishGame", false);
-      } else {
-        this.revealPatch(patch);
-      }
-    },
-
-    revealPatch(patch: any) {
-      if (patch.isFlagged || patch.isRevealed) return;
-
-      patch.isRevealed = true;
-      this.revealedSquareCount++;
-      if (this.revealedSquareCount === this.patches.length - this.bombCount) {
-        this.$emit("finishGame", true);
-      }
-      if (patch.bombCount === 0) {
-        patch.neighbors.forEach((neighbor: any) => {
-          this.revealPatch(neighbor);
-        });
-      }
-    },
-
-    patchLabel(patch: any) {
-      if (patch.isFlagged) return "🚩";
-
-      if (patch.isRevealed === false) return "";
-
-      if (patch.isBomb) return "💣";
-
-      if (patch.bombCount === 0) return "";
-
-      return patch.bombCount;
-    },
-
-    labelClass(patch: any) {
-      if (!patch.isRevealed) return "";
-
-      return "label-" + patch.bombCount;
-    },
-
-    revealAllBombs() {
-      this.bombPositions.forEach((pos) => {
-        if (!this.getPatch(pos.x, pos.y).isFlagged)
-          this.getPatch(pos.x, pos.y).isRevealed = true;
-      });
-    },
-  },
-
-  props: ["height"],
+    props: ["height"],
 });
 </script>
 
 <style scoped>
 .bomb {
-  background-color: red;
+    background-color: red;
 }
 .minefield {
-  --height: 0;
-  --width: calc((var(--num-cols) / var(--num-rows)) * var(--height));
-  --num-cols: 0;
-  --num-rows: 0;
-  display: grid;
-  width: var(--width);
-  height: var(--height);
-  grid-template-columns: repeat(var(--num-cols), 1fr);
-  grid-template-rows: repeat(var(--num-rows), 1fr);
-  gap: 0;
+    --height: 0;
+    --width: calc((var(--num-cols) / var(--num-rows)) * var(--height));
+    --num-cols: 0;
+    --num-rows: 0;
+    display: grid;
+    width: var(--width);
+    height: var(--height);
+    grid-template-columns: repeat(var(--num-cols), 1fr);
+    grid-template-rows: repeat(var(--num-rows), 1fr);
+    gap: 0;
 }
 
 button {
-  border-width: 1px;
-  background-color: gainsboro;
-  --font-percentage: 0.7;
-  font-size: calc(
-    min(
-      var(--height) / var(--num-rows) * var(--font-percentage),
-      var(--width) / var(--num-cols) * var(--font-percentage)
-    )
-  );
-  font-weight: bold;
+    border-width: 1px;
+    background-color: gainsboro;
+    --font-percentage: 0.7;
+    font-size: calc(
+        min(
+            var(--height) / var(--num-rows) * var(--font-percentage),
+            var(--width) / var(--num-cols) * var(--font-percentage)
+        )
+    );
+    font-weight: bold;
 }
 
 button:hover {
-  background-color: grey;
+    background-color: grey;
 }
 
 .revealed {
-  background-color: white;
+    background-color: white;
 }
 
 .label-1 {
-  color: blue;
+    color: blue;
 }
 .label-2 {
-  color: green;
+    color: green;
 }
 .label-3 {
-  color: red;
+    color: red;
 }
 .label-4 {
-  color: purple;
+    color: purple;
 }
 .label-5 {
-  color: maroon;
+    color: maroon;
 }
 .label-6 {
-  color: turquoise;
+    color: turquoise;
 }
 .label-7 {
-  color: black;
+    color: black;
 }
 
 .label-8 {
-  color: grey;
+    color: grey;
 }
 </style>
